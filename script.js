4b582e6b1b1b4e6829d6727bc210982fc74480de
Aniket <aniketkhangar459@gmail.com>
2024-10-08 14:33:12 +0530

    added

---
+++ script.js
@@ -0,0 +1,5 @@
+// Change the message after a few seconds
+setTimeout(() => {
+    document.querySelector('.message').textContent = "You're always in my heart! ❤️";
+    document.querySelector('.sub-message').textContent = "Forever and always...";
+}, 5000);
